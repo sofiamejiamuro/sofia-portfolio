7a94b2fe1289916a4797ab7c4436fa04f7f68bac
sofiamejiamuro <sofiamejiamuro@gmail.com>
2021-04-14 12:50:32 -0500

    m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 // Components
 import Navbar from './components/Navbar';
